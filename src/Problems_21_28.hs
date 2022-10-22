ea40d343896b9af07bfa44acb43259707a18d9f0
mlhoutel <mlhoutel@gmail.com>
2022-10-22 16:17:55 +0200

    Tests 23 - 26

--- src/Problems_21_28.hs
+++ src/Problems_21_28.hs
@@ -15,14 +15,24 @@ myRange s e
     | otherwise = s:(myRange (s+1) e)
     
 -- 23. Extract a given number of randomly selected elements from a list.
-myRandomSelect :: Int -> [a] -> [a]
-myRandomSelect n x = x -- TODO
+-- RANDOM REQUIRE MONADS => Break the tests
 
 -- 24. Lotto: Draw N different random numbers from the set 1..M.
+-- RANDOM REQUIRE MONADS => Break the tests
 
 -- 25. Generate a random permutation of the elements of a list.
+-- RANDOM REQUIRE MONADS => Break the tests
 
 -- 26. Generate the combinations of K distinct objects chosen from the N elements of a list
+
+
+myCombinations :: Int -> [a] -> [[a]]
+myCombinations 0 _ = [[]]
+myCombinations n (x:xs) = start ++ other
+    where   start = [ x:rest | rest <- myCombinations (n-1) xs]
+            other
+                | n <= length xs    = myCombinations n xs
+                | otherwise         = []
 
 -- 27. Group the elements of a set into disjoint subsets.
 
